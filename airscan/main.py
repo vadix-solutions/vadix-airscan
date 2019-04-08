451cf2ddf595ca3aded966936cb09aebd885c6c8
Jake <jake.dixon@vadix.io>
2019-04-07 18:36:36 -0700

    This is to add button functions

--- airscan/main.py
+++ airscan/main.py
@@ -3,6 +3,8 @@ import kivy.uix.boxlayout
 import kivy.uix.textinput
 import kivy.uix.label
 import kivy.uix.button
+import dfgui
+import pandas as pd
 
 
 class SimpleApp(kivy.app.App):
@@ -11,7 +13,7 @@ class SimpleApp(kivy.app.App):
 		
 		self.label = kivy.uix.label.Label(text="Your Message.")
 		self.button = kivy.uix.button.Button(text="Click Me.")
-		self.button.bind(on_press=self.displayMessage)
+		self.button.bind(on_press=self.graph)
 		
 		self.boxLayout = kivy.uix.boxlayout.BoxLayout(orientation="vertical")
 		self.boxLayout.add_widget(self.textInput)
@@ -19,6 +21,10 @@ class SimpleApp(kivy.app.App):
 		self.boxLayout.add_widget(self.button)
 		
 		return self.boxLayout
+	
+	def graph(self,btn):
+		report_dataframe=pd.DataFrame([{"Col1": "sample", "Col2": "test2"}, {"Col1": "sample2", "Col2": "test4"}])
+		dfgui.show(report_dataframe)
 		
 	def displayMessage(self, btn):
 		self.label.text = self.textInput.text
