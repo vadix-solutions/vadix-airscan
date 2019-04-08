cae45becc1b4e0d02fa63af79303b3cd39e808f0
Daniel Vagg <ezeakeal@gmail.com>
2019-04-08 20:34:35 +0100

    Release builds now working (use ./build_release.sh)

--- build_deploy_app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build -t vadix/buildozer .
-docker run --privileged -v $PWD:/buildozer/ -v /dev/bus/usb:/dev/bus/usb -v /etc/udev/rules.d/:/etc/udev/rules.d/ vadix/buildozer buildozer android clean
-docker run --privileged -v $PWD:/buildozer/ -v /dev/bus/usb:/dev/bus/usb -v /etc/udev/rules.d/:/etc/udev/rules.d/ vadix/buildozer buildozer android release deploy run
